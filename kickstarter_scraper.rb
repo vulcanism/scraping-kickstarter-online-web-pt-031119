095ad7f02a0d977439c9d7a656d316ff58940ac7
Savannah C <sgcorrell@gmail.com>
2019-04-17 19:38:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,7 @@ def create_project_hash
   projects = {}
   
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    projects[title.to_sym] = {}
   end
   
   projects
